535164ccd767678c957193a2386a29c2a148199b
felixkwok <64904984+felixkwok@users.noreply.github.com>
2021-05-08 00:04:35 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,10 +1,15 @@
-/*
+
 var gpsPremission = null;
+var gettingData = false;
+var openWeatherMapKey = "74b8a4e9e597a5d689d11a55ddc1405e";
+var geoJSON;
+var map;
+var request;
 
 //set the default location
 localStorage.setItem("defaultLatitude", 22.28552);
 localStorage.setItem("defaultLongitude", 114.15769);
-const userLocation = { 
+var userLocation = { 
 	lat: parseFloat(localStorage.getItem("defaultLatitude")), 
 	lng: parseFloat(localStorage.getItem("defaultLongitude")) 
 };
@@ -65,7 +70,7 @@ function positionError( error ) {
     console.log(gpsPremission);
 }
 
-*/
+
 
 //function that initialize the google map component
 function initMap(){
@@ -73,7 +78,7 @@ function initMap(){
 	console.log(userLocation);
 
   	// The map, centered at latest location
-  	const map = new google.maps.Map(document.getElementById("map"),
+  	map = new google.maps.Map(document.getElementById("map"),
     	{
       		zoom: 10,
       		position: new google.maps.LatLng(userLocation),
@@ -161,6 +166,8 @@ function initMap(){
     	}
   	);
 
+
+
   	//create marker, marked the latest location
   	const marker = new google.maps.Marker({
 		position: new google.maps.LatLng(userLocation),
@@ -220,6 +227,130 @@ function initMap(){
 		sessionStorage.setItem("longitude", lng);
 	})
 
+	//add interaction listeners to make weather requests
+	google.maps.event.addListener(map, 'idle', checkIfDataRequested);
+
+
+}
+// Add the markers to the map
+  var drawIcons = function (weather) {
+     map.data.addGeoJson(geoJSON);
+     // Set the flag to finished
+     gettingData = false;
+  };
+
+  // Clear data layer and geoJSON
+  var resetData = function () {
+    geoJSON = {
+      type: "FeatureCollection",
+      features: []
+    };
+    map.data.forEach(function(feature) {
+      map.data.remove(feature);
+    });
+  };
+
+var checkIfDataRequested = function(){
+	// Stop extra requests being sent
+    while (gettingData === true) {
+      request.abort();
+      gettingData = false;
+    }
+    getCoords();
+  };
+
+// Get the coordinates from the Map bounds
+  var getCoords = function() {
+    var bounds = map.getBounds();
+    var NE = bounds.getNorthEast();
+    var SW = bounds.getSouthWest();
+    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
+  };
+
+// Make the weather request
+var getWeather = function(northLat, eastLng, southLat, westLng) {
+	gettingData = true;
+	var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
+	                    + westLng + "," + northLat + "," //left top
+	                    + eastLng + "," + southLat + "," //right bottom
+	                    + map.getZoom()
+	                    + "&cluster=yes&format=json"
+	                    + "&APPID=" + openWeatherMapKey;
+	request = new XMLHttpRequest();
+	request.onload = proccessResults;
+	request.open("get", requestString, true);
+	request.send();
+};
+
+// Take the JSON results and proccess them
+  var proccessResults = function() {
+    console.log(this);
+    var results = JSON.parse(this.responseText);
+    if (results.list.length > 0) {
+        resetData();
+        for (var i = 0; i < results.list.length; i++) {
+          geoJSON.features.push(jsonToGeoJson(results.list[i]));
+        }
+        drawIcons(geoJSON);
+    }
+  };
+
+// For each result that comes back, convert the data to geoJSON
+var jsonToGeoJson = function (weatherItem) {
+	var feature = {
+	  type: "Feature",
+	  properties: {
+	    city: weatherItem.name,
+	    weather: weatherItem.weather[0].main,
+	    temperature: weatherItem.main.temp,
+	    min: weatherItem.main.temp_min,
+	    max: weatherItem.main.temp_max,
+	    humidity: weatherItem.main.humidity,
+	    pressure: weatherItem.main.pressure,
+	    windSpeed: weatherItem.wind.speed,
+	    windDegrees: weatherItem.wind.deg,
+	    windGust: weatherItem.wind.gust,
+	    icon: "http://openweathermap.org/img/w/"
+	          + weatherItem.weather[0].icon  + ".png",
+	    coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+	  },
+	  geometry: {
+	    type: "Point",
+	    coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+	  }
+};
+
+// Set the custom marker icon
+map.data.setStyle(function(feature) {
+	  return {
+	    icon: {
+	      url: feature.getProperty('icon'),
+	      anchor: new google.maps.Point(25, 25)
+	    }
+	  };
+});
+
+// returns object
+return feature;
+
+
+
+  google.maps.event.addDomListener(window, 'load', initMap);
+};
+
+
+//function for handleing location selecting event 
+function markPlaces(location){
+	console.log("Map clicked");
+	
+	var clickedLocation = new google.maps.LatLng(location);
+
+	marker.setMap(null);
+	const marker = new google.maps.Marker({
+		position: new google.maps.LatLng(userLocation),
+		map: map,
+		icon:'',
+	});
 }
 
 //function for coding the geolocation from the marker's latitude and longitude
@@ -241,19 +372,5 @@ function codeAddress(){
 		 }else {
 		      window.alert("Geocoder failed due to: " + status);
 			}
-	});
-}
-
-//function for handleing location selecting event 
-function markPlaces(location){
-	console.log("Map clicked");
-	
-	var clickedLocation = new google.maps.LatLng(location);
-
-	marker.setMap(null);
-	const marker = new google.maps.Marker({
-		position: new google.maps.LatLng(userLocation),
-		map: map,
-		icon:'',
 	});
 }
