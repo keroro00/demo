b750c51658f5f4ffb8d1b08d40ba79ad4ed9c9ad
keroro00 <82613217+keroro00@users.noreply.github.com>
2021-05-10 15:54:29 +0800

    change to forecast 7 days

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var userLocation = {
     lat: parseFloat(localStorage.getItem("defaultLatitude")), 
     lng: parseFloat(localStorage.getItem("defaultLongitude")) 
 };
-
+var Weekarray =[];
 var Maxarray =[];
 var Minarray =[];
 var latitudeOfStation = [];
@@ -282,7 +282,32 @@ function addRowForecast(forecast) {
     var forecastWeek = document.getElementById("forecastWeek");
     var Weekrow = forecastWeek.insertCell();
     Weekrow.setAttribute('data-label', "week");
-    Weekrow.innerHTML = forecast.week;
+        var week='';
+    switch (forecast.week){
+        case 'Monday':
+            week = "Mon";
+            break;
+        case 'Tuesday':
+            week = "Tue";
+            break;
+        case 'Wednesday':
+            week = "Wed";
+            break;  
+        case 'Thursday':
+            week = "Thur";
+            break; 
+        case 'Friday':
+            week = "Fri";
+            break;
+        case 'Saturday':
+            week = "Sat";
+            break;
+        case 'Sunday':
+            week = "Sun";
+            break;
+    }
+    Weekrow.innerHTML =week;
+    Weekarray.push(week);
 
     var forecastMaxTemp = document.getElementById("forecastMaxTemp"); //highest temp
     var MaxTemprow = forecastMaxTemp.insertCell();
@@ -422,7 +447,7 @@ function updateLocation(){
         }
 }
 function showChart(){
-  var xValues = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+  var xValues = Weekarray;
   var yValues = Maxarray;
   var yValues2 = Minarray;
 
