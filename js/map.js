541dbc3e8a730f8052bb2badc76f1c81bfa972f9
felixkwok <felixkwok1026@gmail.com>
2021-05-10 22:49:15 +0800

    bug fix

--- js/map.js
+++ js/map.js
@@ -203,13 +203,31 @@ function initMap(){
 	locationButton.classList.add("custom-map-control-button");
 	map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 	locationButton.addEventListener("click", function (){
-		
+		if(gpsPermission == 'false'){
+			console.log("Getting permission");
 			navigator.geolocation.getCurrentPosition(resetLocation, positionError, {
             maximumAge: 30000, 
             timeout: 10000, 
             enableHighAccuracy: true});
+            sessionStorage.setItem("gpsPermission", true);
 	        updateLocation();
 	        retrieveWeather();
+		}else if(gpsPermission == 'true'){
+			navigator.geolocation.getCurrentPosition( function(position){
+				//get the latitude and longitude
+			    latitude = position.coords.latitude;
+			    longitude = position.coords.longitude;
+
+			    //save the current location for offline use
+			    localStorage.setItem("latitude", latitude);
+			    localStorage.setItem("longitude", longitude);
+			    updateLocation();
+	        	retrieveWeather();
+			}, function(err){
+				console.log(err);
+			});
+		}
+			
 	});
 
   	//create marker, marked the latest location
@@ -320,6 +338,9 @@ function geoCoding(userLocation){
     }
     else {
       console.log("Geocoder failed due to: " + status);
+      if(status == 'OVER_QUERY_LIMIT'){
+      		display.innerHTML ="<font color='red'>OVER_REQUEST_LIMIT</font>";
+      }
       display.innerHTML ="<font color='red'>Not a valid location</font>";
 	}
 	});
